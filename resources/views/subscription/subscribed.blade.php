d0e6691c7079cebc3d0fb032e4900e8b38d2c670
ianbrucey <ib708090@gmail.com>
2018-01-16 07:16:21 -0800

    adding variables for "thank you for subscribing page"

--- resources/views/subscription/subscribed.blade.php
+++ resources/views/subscription/subscribed.blade.php
@@ -1,30 +1,34 @@
 @extends('layouts.app')
 
 @section('body')
-    @php
-    $planName = "nail color and fill";
-    $price = 7000;
-    $interval = "month";
-    @endphp
-
-    <style>
-    </style>
-    <div class="container-fluid">
-        <a class="btn btn-info" href="{{ URL::previous() }}">Back</a>
-    </div>
+    @if(!session('planName'))
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 offset-md-4 text-center">
+                    <h1>Why are you here?</h1>
+                    <h2 class="btn btn-primary">
+                        Dave Chappelle.....GO HOME!!!
+                    </h2>
+                </div>
+            </div>
+        </div>
+    @else
     <div class="container">
         <div class="row">
             <div class="col-md-8 offset-md-2">
                 <div class="card card-header">
-                    <h1 class="text-center">Thanks for your subscription to {{$planName}}</h1>
+                    <h1 class="text-center">Thanks for your subscription to {{session('planName')}}</h1>
                 </div>
                 <div class="card card-body">
-                    <h2><u>Details</u></h2>
-                    <h3 class="text-center">Price: <strong>{{$price}}</strong> per <strong>{{$interval}}</strong></h3>
-                    <a href="" class="btn btn-primary float-left">Go to my subscriptions</a>
-                    <a href="" class="btn btn-primary float-right">Find more services</a>
+                    <h2 class="text-center"><u>Details</u></h2>
+                    <h3 class="text-center">Price: <strong>{{formatPrice(session('price'))}}</strong> per <strong>{{session('interval')}}</strong></h3>
+                    <hr>
+                    <p class="text-center">To use this service now, <a href="/account/mysubscriptions">click here to go to your subscriptions</a></p>
+                    <a href="/home" class="btn btn-primary">Find more services</a>
                 </div>
             </div>
         </div>
+    </div>
+    @endif
 
 @endsection
