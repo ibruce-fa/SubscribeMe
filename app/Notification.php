18915b6fe349c1c4536b56f0f09b0b8d27452725
ianbrucey <ib708090@gmail.com>
2018-03-07 18:59:29 -0800

    Update Notification.php
    incomplete notification method

--- app/Notification.php
+++ app/Notification.php
@@ -5,17 +5,19 @@ namespace App;
 use App\Mail\ConfirmAccount;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
 
 class Notification extends Model
 {
     /** notification types */
-    const CONFIRM_ACCOUNT_TYPE  = 1;
-    const NEW_SUBSCRIPTION_TYPE = 2;
-    const NEW_USER_TYPE         = 3;
-    const SUPPORT               = 4;
-    /** notification types */
-    /** email types */
+    const SUPPORT_NOTIFICATION                  = ['notification_id'=>1, 'name'=>'support', 'type'=>'','subject'=>'','body'=>''];
+    const SUPPORT_ACKNOWLEDGE_NOTIFICATION      = ['notification_id'=>1, 'name'=>'support', 'type'=>'','subject'=>'','body'=>''];
+    const SUPPORT_RESPONSE_NOTIFICATION         = ['notification_id'=>1, 'name'=>'support', 'type'=>'','subject'=>'','body'=>''];
+    const WELCOME_USER_NOTIFICATION             = ['notification_id'=>2, 'name'=>'welcome_user'];
+//    const SUPPORT_NOTIFICATON          = ['notification_id'=>3, 'name'=>'WELCOME_USER'];
+//    const NEW_SUBSCRIPTION_TYPE = 2;
+//    const SUPPORT               = 4;
 //    const CONFIRM_ACCOUNT_TYPE  = 1;
 //    const NEW_SUBSCRIPTION_TYPE = 2;
 //    const NEW_USER_TYPE         = 3;
@@ -38,8 +40,33 @@ class Notification extends Model
         $this->subject = $request->get('subject');
         $this->body = $request->get('body');
         $this->save();
+        
+    }
 
-
+    public function sendNotification(Request $request = null, $notificationType, User $user)
+    {
+        if($notificationType == self::WELCOME_USER_NOTIFICATION['notification_id']) {
+            $this->type     = self::WELCOME_USER_NOTIFICATION['type'];
+            $this->subject  = self::WELCOME_USER_NOTIFICATION['subject'];
+            $this->body     = self::WELCOME_USER_NOTIFICATION['body'];
+            $this->recipient_id = Auth::id();
+            return $this->save();
+        } elseif ($notificationType == self::SUPPORT_NOTIFICATION['notification_id']) {
+            $this->type         = $request->get('type');
+            $this->recipient_id = $request->get('recipient_id') ?: 0;
+            $this->subject      = $request->get('subject');
+            $this->body         = $request->get('body');
+            $this->save(); // sends to us
+            
+            $userNotification           = new Notification(); // sends to the user 
+            $userNotification->type     = self::SUPPORT_ACKNOWLEDGE_NOTIFICATION['type'];
+            $userNotification->subject  = self::SUPPORT_ACKNOWLEDGE_NOTIFICATION['subject'];
+            $userNotification->body     = self::SUPPORT_ACKNOWLEDGE_NOTIFICATION['body'];
+            $userNotification->recipient_id = Auth::id();
+            $userNotification->save();
+            return true;
+        }
+        return false;
     }
 
     public function sendEmail($emailType, User $user = null, $dataArr = [])
