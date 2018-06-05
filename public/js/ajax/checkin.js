8a68a15fba4ea4779a8e1cbbeb4ffae4b0fe29bc
ianbrucey <ib708090@gmail.com>
2018-06-05 16:34:41 -0700

    cleaning up the checkin view

--- public/js/ajax/checkin.js
+++ public/js/ajax/checkin.js
@@ -1,6 +1,6 @@
 $('.checkin').on('click', function () {
     let zis     = $(this);
-    let planId = zis.attr('data-plan-id');
+    let planId  = zis.attr('data-plan-id');
 
     let subscriptionId = zis.attr('data-subscription-id');
     let url = "/subscription/checkin/"+planId+"/"+subscriptionId;
@@ -12,7 +12,6 @@ $('.checkin').on('click', function () {
 
     $.post(url, postdata).done(function (data) {
         checkinModal.find('.checkin-code').text(data);
-        zis.html("code: "+ data);
     });
 
 });
--- resources/views/modals/custom/checkin-modal.blade.php
+++ resources/views/modals/custom/checkin-modal.blade.php
@@ -1,14 +1,14 @@
 <div id="checkin-{{$subscription->id}}" class="sm-modal" role="dialog" tabindex="-1" aria-hidden="true">
-    <div class="modal-content">
+    <div class="modal-content col-md-6 offset-md-3 theme-background">
         <div class="modal-header">
-            <button type="button" class="close hide-sm-modal" data-dismiss="modal">&times;</button>
+            <button type="button" class="close btn bg-white hide-sm-modal mt-2 mb-2" data-dismiss="modal">Done</button>
         </div>
-        <div class="modal-body">
+        <div class="modal-body text-white-children">
             <h1 class="text-center">Check-in</h1>
             <div id="checkin-response-container">
                 <h3 class="text-center">Give this 5 digit code to the service worker to use their service.</h3>
                 <hr>
-                <h1 class="text-center checkin-code">{{getLoadingAnimation()}}</h1>
+                <h1 class="text-center checkin-code bg-white theme-color">{{getLoadingAnimation()}}</h1>
             </div>
         </div>
     </div>
